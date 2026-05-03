445654d448f65a0327e967f41736dad02b6222d5
Bouali <youssefbouali206@gmail.com>
2026-05-03 19:34:18 +0100

    Up

--- vibraguard/frontend/client/components/dashboard/VibrationChart.tsx
+++ vibraguard/frontend/client/components/dashboard/VibrationChart.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { useVibrations } from "@/hooks/use-vibrations";
 import { useMoteurs } from "@/hooks/use-moteurs";
 import { formatTime } from "@/lib/utils";
