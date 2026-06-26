4567be82aed373a55700c1cc2c6d5a94cd877850
Youssef Bouali <youssefbouali206@gmail.com>
2026-06-26 10:07:25 +0100

    e

---
+++ auto-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/ubuntu/.VibraGuard/ || exit
+
+git fetch origin main
+
+LOCAL=$(git rev-parse main)
+REMOTE=$(git rev-parse origin/main)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "New changes detected, pulling..."
+    git pull origin main
+
+    minikube delete && sh run-all.sh 
+fi
